2fe6e42622cd303a4bc1299765f52aceff8a82be
bradserlis <bradserlis@yahoo.com>
2019-10-04 14:04:22 -0700

    Add basic auth install blaze UI components for auth wrap blaze components in React component configure username / email field to simply usernames

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -4,6 +4,7 @@ import { withTracker } from 'meteor/react-meteor-data';
 
 import { Tasks } from '../api/tasks.js';
 import Task from './Task.js';
+import AccountsUIWrapper from './AccountsUIWrapper';
 
 // App component - represents the whole app
 class App extends Component {
@@ -61,6 +62,8 @@ class App extends Component {
                     />
                     Hide Completed Tasks
                 </label>
+
+                <AccountsUIWrapper />
 
                 <form className="new-task" onSubmit={this.handleSubmit} >
                     <input
